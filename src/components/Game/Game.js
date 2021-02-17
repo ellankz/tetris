063a24f25d3ae18b00bab9bbf2548eb8c95d4f9c
ellankz <enelipa@gmail.com>
2021-02-17 14:53:28 +0200

    feat: shape rotate and row clear

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -32,6 +32,7 @@ function Game() {
     ['#','0','0','0','0','0','0','0','0','0','0','#'],
     ['#','#','#','#','#','#','#','#','#','#','#','#'],
   ];
+
   const [fieldState, setFieldState] = useState(cloneMatrix(initialFieldState));
 
   const [fieldWithShape, setFieldWithShape] = useState(cloneMatrix(initialFieldState));
@@ -41,7 +42,6 @@ function Game() {
   const [shapePosition, setShapePosition] = useState({x: FIELD_CENTER_X, y: 0});
 
   const [direction, setDirection] = useState(null);
-
 
   function getRandomColor() {
     const colors = ['r', 'g', 'b', 'p', 'c', 'y'];
@@ -93,11 +93,10 @@ function Game() {
   let keyPressTime = useRef();
   keyPressTime.current = new Date().getTime();
 
-
   useEffect(() => {
-    function updateField (newPos) {
-      if (!newPos) return;
-      if (newPos.x === shapePosition.x && newPos.y === shapePosition.y) return;
+    function updateField (newPos, rotated) {
+      if ((!newPos && !rotated)
+      || (!rotated && newPos.x === shapePosition.x && newPos.y === shapePosition.y)) return;
       let field = cloneMatrix(fieldWithShape);
   
       function clearShape() {
@@ -105,31 +104,33 @@ function Game() {
       }
   
       function drawShape() {
-        for (let i = 0; i < fallingShape.length; i++) {
-          for (let u = 0; u < fallingShape[0].length; u++) {
-            if (fallingShape[i][u] !== '0') {
-              field[newPos.y + i][newPos.x + u] = fallingShape[i][u];
+        const shape = rotated || fallingShape;
+        const pos = newPos || shapePosition;
+        for (let i = 0; i < shape.length; i++) {
+          for (let u = 0; u < shape[0].length; u++) {
+            if (shape[i][u] !== '0') {
+              field[pos.y + i][pos.x + u] = shape[i][u];
             }
           }
         }
       }
-      
+
       clearShape();
       drawShape();
       setFieldWithShape(field);
-
     };
 
-    function detectCollision(newPos) {
-      if (!newPos) return false;
-      const {x, y} = newPos;
-      if (x < 1 || x + fallingShape[0].length > RIGHT_WALL_X || y >=  BOTTOM_WALL_Y) {
+    function detectCollision(newPos, rotated) {
+      if (!newPos && !rotated) return false;
+      const {x, y} = newPos || shapePosition;
+      const shape = rotated || fallingShape;
+      if (x < 1 || x + shape[0].length > RIGHT_WALL_X || y >=  BOTTOM_WALL_Y) {
         return true;
       }
       function mapShapeToLayedTiles() {
-        for (let i = y; i < y + fallingShape.length; i ++) {
-          for (let u = x; u < x + fallingShape[0].length; u++) {
-            if (fallingShape[i - y][u - x] !== '0') {
+        for (let i = y; i < y + shape.length; i++) {
+          for (let u = x; u < x + shape[0].length; u++) {
+            if (shape[i - y][u - x] !== '0') {
               if (fieldState[i][u] !== '0') {
                 return true;
               }
@@ -168,24 +169,77 @@ function Game() {
 
       return newPos;
     }
-    
-    function handleKey (event) {
-      const time = new Date().getTime();
-      if (time - keyPressTime < 500) return;
-      keyPressTime.current = time;
-      if (event.key === 'ArrowRight'){
-        setDirection('right');
-      } else if (event.key === 'ArrowLeft'){
-        setDirection('left');
-      } else if (event.key === 'ArrowDown'){
-        setDirection('down');
+
+    function rotateShape() {
+      const shape = [];
+
+      for (let i = 0; i < fallingShape[0].length; i++) {
+        const row = [];
+        shape.push(row);
+        for (let u = (fallingShape.length - 1); u >= 0; u--) {
+          row.push(fallingShape[u][i]);
+        }
       }
+
+      const collided = detectCollision(undefined, shape);
+
+      if (!collided) {
+        setFallingShape(shape);
+        updateField(undefined, shape);
+      }
+
     }
 
-    function dropNewShape () {
+    function dropNewShape() {
       setFieldState(cloneMatrix(fieldWithShape));
       setFallingShape(createRandomShape(getRandomColor()));
       setShapePosition({x: FIELD_CENTER_X, y: 0});
+    }
+
+    function speedDropShape() {
+      setDirection('down');
+    }
+
+    function handleKeyUp(event) {
+      const time = new Date().getTime();
+      if (time - keyPressTime < 500) return;
+      keyPressTime.current = time;
+      if (event.key === 'ArrowRight') {
+        setDirection('right');
+      } else if (event.key === 'ArrowLeft') {
+        setDirection('left');
+      } else if (event.key === 'ArrowDown') {
+        setDirection('down');
+      } else if (event.key === 'ArrowUp')
+        rotateShape();
+    }
+
+    function handleKeyDown(event) {
+      if (event.keyCode === 32) {
+        speedDropShape();
+      }
+    }
+
+    function checkForFilledLine() {
+      const filledLines = [];
+      for (let i = 0; i < fieldWithShape.length - 1; i++) {
+        if (fieldWithShape[i].every((tile) => tile !== '0')) {
+          filledLines.push(i);
+        }
+      }
+      return filledLines;
+    }
+
+    function clearLine(lineIndexes) {
+      if (!lineIndexes.length) return;
+      const field = [];
+      lineIndexes.forEach(() => field.push([...initialFieldState[0]]));
+      for (let i = 0; i < fieldWithShape.length; i++) {
+        if (lineIndexes.every(index => index !== i)) {
+          field.push(fieldWithShape[i]);
+        }
+      }
+      setFieldState(field);
     }
     
     if (direction) {
@@ -195,19 +249,25 @@ function Game() {
 
       if ((isCollided && direction === 'down') || shapePosition.y >= BOTTOM_WALL_Y - fallingShape.length) {
         dropNewShape();
+        const filledRows = checkForFilledLine();
+        if (filledRows.length) clearLine(filledRows);
       }
       
       if (!isCollided && (shapePosition.x !== newPos.x || shapePosition.y !== newPos.y)){
         updateField(newPos);
       }
+
     }
 
-    document.addEventListener('keyup', handleKey);
+    document.addEventListener('keyup', handleKeyUp);
+
+    document.addEventListener('keydown', handleKeyDown);
     
     return () => {
-      document.removeEventListener('keyup', handleKey);
+      document.removeEventListener('keyup', handleKeyUp);
+      document.removeEventListener('keydown', handleKeyDown);
     };
-  }, [shapePosition, direction, fallingShape, fieldState, fieldWithShape]);
+  }, [shapePosition, direction, fallingShape, fieldState, fieldWithShape, initialFieldState]);
 
   useEffect(() => {
     const interval = setInterval(() => {
